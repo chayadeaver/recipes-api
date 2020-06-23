135e2cad2dc8cdc839fa061f8eff0938b40bc194
chayadeaver <chayadeaver@gmail.com>
2020-06-22 22:09:36 -0400

    seeded with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Recipe.destroy_all
+Ingredient.destroy_all
+
+
+
+3.times do 
+    User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+end
+
+50.times do 
+    User.all.sample.recipes.create(name: Faker::Food.dish, image_url: "sample.jpg", description: Faker::Food.description, instructions: "#{Faker::Food.measurement} #{Faker::Food.ingredient} #{Faker::Food.measurement} #{Faker::Food.vegetables}")
+end
+
+100.times do 
+    User.all.sample.recipes.all.sample.ingredients.create(name: Faker::Food.ingredient, quantity: Faker::Number.number(digits: 2), unit: Faker::Food.metric_measurement)
+end
