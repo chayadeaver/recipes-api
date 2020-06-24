8c3aa9698ee4bb1b22f4b1136bdf28902806d2e5
chayadeaver <chayadeaver@gmail.com>
2020-06-24 19:43:43 -0400

    add namspace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  resources :likes
+  resources :comments
+  resources :ingredients
+  resources :ingredient_recipes
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
+  # want api to be domain.com/api/v1/resource
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :recipes
+      end
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
