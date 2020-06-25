c6ebb6bae23aede6465b98776d697d2bd07b037c
chayadeaver <chayadeaver@gmail.com>
2020-06-24 20:45:14 -0400

    add comments

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :description, :instructions, :ingredients
+  attributes :name, :image_url, :description, :instructions, :ingredients, :comments
 end
