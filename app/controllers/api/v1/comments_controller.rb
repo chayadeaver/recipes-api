cc9e081be9c2a62f9d053cae6bf80e89e060eb93
chayadeaver <chayadeaver@gmail.com>
2020-06-24 20:46:01 -0400

    add index action

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index
+        @comments = Comment.all 
+        render json: CommentSerializer.new(@comments).serializable_hash, status: :ok
+    end
+
+
 end
