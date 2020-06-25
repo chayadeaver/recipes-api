6d56f038f8d12161e87a3c0939e580548e24f2c6
chayadeaver <chayadeaver@gmail.com>
2020-06-24 20:47:24 -0400

    add two auth methods with dummy code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    helpers do
+        def current_user
+            # mocked version of being logged in
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
