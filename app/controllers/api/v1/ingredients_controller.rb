b5da60b197e1f087ffd0473766b25a4741c50f4b
chayadeaver <chayadeaver@gmail.com>
2020-06-24 20:45:49 -0400

    added index and show

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::IngredientsController < ApplicationController
     
+    def index
+        @ingredients = Ingredient.all 
+        render json: IngredientSerializer.new(@ingredients).serialized_json, status: :ok
+    end
+
+    def show
+        @ingredient = find_by(id: params[:id])
+        render json: IngredientSerializer.new(@ingredient).serialized_json, status: :ok
+    end
 end
