9dcb36a559a408ea2627297244ccc25155d1416b
chayadeaver <chayadeaver@gmail.com>
2020-06-25 22:30:23 -0400

    rearranged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,19 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
-  namespace :api do
-    namespace :v1 do
-      resources :ingredients
-    end
-  end
-  resources :ingredient_recipes
-  namespace :api do
-    namespace :v1 do
-      resources :recipes do
-        resources :ingredients, :comments, :likes
-      end
-    end
-  end
-  # want api to be domain.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :users do
         resources :recipes, :comments, :likes
       end
+      resources :ingredients
+      resources :recipes do
+        resources :ingredients, :comments, :likes
+      end
+      resources :likes
+      resources :comments
+      resources :ingredient_recipes
     end
   end
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
