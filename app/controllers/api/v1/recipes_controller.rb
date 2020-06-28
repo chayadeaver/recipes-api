3d136d126611e4955abe8108019c1fb734230dba
chayadeaver <chayadeaver@gmail.com>
2020-06-28 15:59:46 -0400

    created custom action for current user's recipes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,6 +4,17 @@ class Api::V1::RecipesController < ApplicationController
         render json: RecipeSerializer.new(@recipes)
     end
 
+    def current_user_recipes
+        if logged_in?
+            @recipes = current_user.recipes
+            render json: RecipeSerializer.new(@recipes)
+        else
+            render json: {
+                notice: "You don't have any recipes, yet."
+            }
+        end
+    end
+
     def show
         @recipe = Recipe.find_by(id: params[:id])
         render json: RecipeSerializer.new(@recipe).serializable_hash
