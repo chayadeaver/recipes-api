f9dd33e00167a5a9eb227a1e8981e2158157372d
chayadeaver <chayadeaver@gmail.com>
2020-06-27 22:57:03 -0400

    added custom attributes for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,11 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email, :recipes, :comments, :likes
 
+  attribute :users_show_recipes do |user|
+    user.recipes.map{|recipe| {
+      recipe_name: recipe.name,
+      recipe_image: recipe.image_url
+    }}
+  end
+
 end
