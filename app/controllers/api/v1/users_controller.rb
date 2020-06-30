3bf7efb661468256e9951fd16dab98ce5ae77cd2
chayadeaver <chayadeaver@gmail.com>
2020-06-29 22:05:30 -0400

    add serializer and session[:user_id]

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,9 +13,13 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: :created, location: @user
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user), status: :created, status: :created
         else
-            render json: @user.errors, status: :unprocessable_entity
+            resp = {
+                error: @user.errors.full_messages.to_sentence
+            }
+            render json: resp, status: :unprocessable_entity
         end
     end
 
