89def19741c00083e92ea9d6dd8f96223c986855
chayadeaver <chayadeaver@gmail.com>
2020-07-04 14:29:01 -0400

    have this controller manage ingredient creation

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
+    # before_action :logged_in?, except [:index, :show]
+
     def index
         @recipes = Recipe.all 
         render json: RecipeSerializer.new(@recipes)
@@ -21,15 +23,21 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     def create
-        @recipe = Recipe.new(recipe_params)
+        # byebug
+        @recipe = current_user.recipes.new(recipe_params)
         if @recipe.save
-            render json: RecipeSerializer.new(@recipe).serializable_hash
+            render json: RecipeSerializer.new(@recipe).serializable_hash, status: :created
+        else
+            error_resp = {
+                error: @recipe.errors.full_messages.to_sentence
+            }
+            render json: error_resp, status: :unprocessable_entity
         end
     end
 
 private
 
     def recipe_params
-        params.require(:recipe).permit(:name, :image_url, :description, :instructions, :user_id)
+        params.require(:recipe).permit(:name, :image_url, :description, :instructions, :user_id, ingredients_attributes: [:name, :quantity, :unit])
     end
 end
