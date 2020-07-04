0c141d1957ea8ca011f3a26c1680db583e161d82
chayadeaver <chayadeaver@gmail.com>
2020-07-04 14:28:40 -0400

    set create action to include recipe wi/ ingredient

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -9,4 +9,26 @@ class Api::V1::IngredientsController < ApplicationController
         @ingredient = find_by(id: params[:id])
         render json: IngredientSerializer.new(@ingredient).serialized_json, status: :ok
     end
+
+    def create
+        if params[:recipe_id]
+            @recipe = Recipe.find_by(recipe_id: params[:recipe_id])
+            @ingredient = @recipe.ingredients.build(ingredient_params)
+            if @ingredient.save
+                render json: IngredientSerializer.new(@ingredient).serialized_json, status: :ok
+            else
+                error_resp = {
+                    error: @ingredient.errors.full_messages.to_sentence
+                }
+                render json: error_resp, status: :unprocessable_entity
+            end
+        end
+        
+    end
+
+private
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name, :quanity, :unit, :recipe_id)
+    end
 end
