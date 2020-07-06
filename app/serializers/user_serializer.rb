00bc9328572936d599961a4c4501624655ee2e2c
chayadeaver <chayadeaver@gmail.com>
2020-07-05 21:10:13 -0400

    added has_many

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,17 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email, :comments, :likes
+  has_many :recipes
 
-  attribute :recipes do |user|
-    user.recipes.map{|recipe| {
-      id: recipe.id,
-      name: recipe.name,
-      image: recipe.image_url,
-      description: recipe.description,
-      instructions: recipe.instructions
+  # attribute :recipes do |user|
+  #   user.recipes.map{|recipe| {
+  #     id: recipe.id,
+  #     name: recipe.name,
+  #     image: recipe.image_url,
+  #     description: recipe.description,
+  #     instructions: recipe.instructions
 
-    }}
-  end
+  #   }}
+  # end
 
 end
