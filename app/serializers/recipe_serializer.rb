3ae28718951a0bc95c2e276254fa41aa094869a6
chayadeaver <chayadeaver@gmail.com>
2020-07-05 21:11:13 -0400

    added relationships

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,22 +1,7 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :description, :instructions, :comments, :likes
+  attributes :name, :image_url, :description, :instructions, :user_id, :comments, :likes
+  has_many :ingredients
+  belongs_to :user
 
-  attribute :user do |recipe|
-    {
-      id: recipe.user.id,
-      name: recipe.user.name
-    }
-  end
-
-  attribute :ingredients do |recipe|
-    recipe.ingredients.map do |ingredient|
-      {
-        id: ingredient.id,
-        name: ingredient.name,
-        quantity: ingredient.quantity,
-        unit: ingredient.unit
-      }
-    end
-  end
 end
