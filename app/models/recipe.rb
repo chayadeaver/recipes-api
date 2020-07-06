17931cb25c7cb9b293baa63b5f6c56e709f92e7e
chayadeaver <chayadeaver@gmail.com>
2020-07-05 21:10:37 -0400

    removed join table associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,7 @@ class Recipe < ApplicationRecord
     belongs_to :user, optional: true
     has_many :comments
     has_many :likes
-    has_many :ingredient_recipes, dependent: :destroy
-    has_many :ingredients, through: :ingredient_recipes
+    has_many :ingredients
     validates :name, :image_url, :description, :instructions, presence: true
 
     accepts_nested_attributes_for :ingredients
