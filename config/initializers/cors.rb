33c6d67edb9567e65d7326e4ff87c1b4de4e669d
chayadeaver <chayadeaver@gmail.com>
2020-11-05 15:02:23 -0500

    changed origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'myfamilyrecipes.netlify.app'
+        origins 'https://myfamilyrecipes.netlify.app'
 
         resource '*',
             headers: :any,
